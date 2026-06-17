bb205208c43db903e702a764ddf3de9f63ba2ec9
Tom Oinn <tomoinn@crypticsquid.com>
2026-06-17 10:19:44 +0100

    Disable Export to SVG button in ActionBar. SVG export is slightly broken so better to not have the option, most SVG software can import from PDF anyway and we can save to PDF through system save

--- src/components/ActionBar.vue
+++ src/components/ActionBar.vue
@@ -48,7 +48,7 @@ defineEmits<{
       </svg>
       Save
     </button>
-    <button class="primary" @click="$emit('exportToSVG')">
+    <button v-if="false" class="primary" @click="$emit('exportToSVG')">
       <svg viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
         <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
         <circle cx="8.5" cy="8.5" r="1.5"></circle>
